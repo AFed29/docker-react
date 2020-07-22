86c78b7ba364692f2a875a1253c30f4482435b26
Alex Fedenczuk <alex.fedenczuk@gmail.com>
2020-07-22 18:04:55 +0100

    added travis ci deploy flow

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          This be cool and will change
+          First Attempt Of deploying!
         </p>
         <a
           className="App-link"
